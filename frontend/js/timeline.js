c97d68bc0c7b49ec9210c03ead1b0482ef482676
RO\ASpirido <andra01spiridon@yahoo.com>
2017-04-24 16:56:34 +0300

    [FrontEnd] Timeline Memories

--- frontend/js/timeline.js
+++ frontend/js/timeline.js
@@ -1,4 +1,22 @@
-$(document).ready(function() {
+﻿var _this = this;
+var link = $('#navbar a.dot');
+function scrNav() {
+    var sTop = $('.right-main').scrollTop();
+    $('.slider-inside').each(function () {
+        var id = $(this).attr('id'),
+            offset = $(this).offset().top - 1,
+            height = $(this).height();
+        console.log(id);
+        console.log(offset);
+        console.log(height);
+        if (sTop >= offset && sTop < offset + height) {
+            link.removeClass('active');
+            $('#navbar').find('[data-scroll="' + id+ '"]').toggleClass('active');
+        }
+    });
+
+}
+$(document).ready(function () {
     $('.icon-container i').click(function(){
         $(this).parents('.left-container').find('.left-inside').toggleClass("hide");
         $(this).parents('.left-container').find('.hided-inside').toggleClass("show");
@@ -13,4 +31,22 @@ $(document).ready(function() {
     $('.custom-notification').click(function(){
         $(this).find('.notification-dropdown').toggleClass("show");
     });
+   
+
+
+    
+
+    link.on('click', function (e) {
+        var target = $($(this).attr('href'));
+        $('.right-main').animate({
+            scrollTop: target.offset().top
+        }, 732);
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+
 });
+
+
+
+
